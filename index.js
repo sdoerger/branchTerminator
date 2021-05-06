0b5f61ded64810c8e8c0325b8ddf88882f85e5a2
stefandoerger <doerger.stefan@gmail.com>
2021-05-06 15:26:08 +0200

    Add prompt

--- index.js
+++ index.js
@@ -1,18 +1,66 @@
 // require the library, main export is a function
 const { clear } = require('console'),
   simpleGit = require('simple-git'),
+  prompt = require('prompt-sync')()
   // repoPath = '/home/mypath/to/my/git-repository',
-  repoPath = '/home/stefan/Develop/wl/archive/sprintScript',
-  git = simpleGit(repoPath, { binary: 'git' }),
-  branches = ['test-2'],
-  option = 'both';
+  // repoPath = '/home/stefan/Develop/wl/archive/sprintScript',
+  // branches = ['test1234'],
+  // option = 'both';
 // options: 'local', 'remote', 'both'
 
+// ########################
+// console navigation Start
+// ########################
+
 clear();
+ 
+console.log(`
+Please enter the exact path to your repo.
+I. e.: /home/mypath/to/my/git-repository
+`);
+const repoPatha = prompt();
+console.log(`
+This is your your path: ${repoPatha}
+`);
+
+console.log(`
+Please enter a list of branches you intend to delete (in one line).
+I. e.: test1, test2, test3
+`);
+const repoBranches = prompt();
+
+splitRepoBranches = repoBranches.split(', ')
+console.log(`
+These are your the branches you want to delete: ${splitRepoBranches}
+`);
+
+console.log(`
+Please Choose, where you want to delete
+1) local
+2) remote
+3) both
+`);
+const repoOption = prompt();
+
+if (!(repoOption == '1' || repoOption == '2' || repoOption == '3')) {
+  console.log('Wrong option')
+  console.log('Bye bye')
+  process.exit(1)
+}
+
+// ########################
+// console navigation End
+// ########################
+
+const git = simpleGit(repoPatha, { binary: 'git' }),
+transformedOptions = {
+  '1': 'local',
+  '2': 'remote', 
+  '3': 'both',
+}
+
 
 async function deleteBranch(branch, option) {
-  console.log('... searching for branches ...')
-  
   const errorMessage = (branch, type, extra = '') =>
     console.log(
       `Error: Branch ${branch} does not exist ${type}${extra}.`
@@ -47,7 +95,7 @@ async function deleteBranch(branch, option) {
   }
 }
 
-for (const branch of branches) {
+for (const branch of repoBranches) {
   // console.log(branch)
-  deleteBranch(branch, option);
+  deleteBranch(branch, transformedOptions[repoOption]);
 }
