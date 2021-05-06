f14480568a6c8c1e616dce104ce428204bdacbc5
stefandoerger <doerger.stefan@gmail.com>
2021-05-06 14:09:29 +0200

    Optimize handling and error messages

--- index.js
+++ index.js
@@ -1,36 +1,51 @@
 // require the library, main export is a function
-const { clear } = require("console"),
-simpleGit = require('simple-git'),
-repoPath = '/home/stefan/Develop/wl/archive/sprintScript',
-git = simpleGit(repoPath, { binary: 'git' }),
+const { clear } = require('console'),
+  simpleGit = require('simple-git'),
+  // repoPath = '/home/mypath/to/my/git-repository',
+  repoPath = '/home/stefan/Develop/wl/archive/sprintScript',
+  git = simpleGit(repoPath, { binary: 'git' }),
+  branches = ['test1234'],
+  option = 'both';
+// options: 'local', 'remote', 'both'
 
-// localBranches = ['test1', 'test2', 'test3', 'test4'],
-// remoteBranches = ['test1', 'test2', 'test3', 'test4'],
-branches = [];
+clear();
 
-clear()
+async function deleteBranch(branch, option) {
+  const errorMessage = (branch, type, extra = '') =>
+    console.log(
+      `Error: Branch ${branch} does not exist ${type}${extra}.`
+    );
+  const successMessage = (branch, type) =>
+    console.log(`${type} branch \'${branch}\' is deleted`);
 
-async function deleteBranch (branch) {
+  const remote = 'remote'
   try {
-  // await git.pull()
-  // await git.deleteLocalBranches('to-deletely')
-  await git.deleteLocalBranch(branch, true)
-  console.log(`Local branch \'${branch}\' is deleted`)
-  await git.push('origin', branch, ['-d'])
-  console.log(`Remote branch \'${branch}\' is deleted`)
-  
-  console.log('Done')
-  
+    const remote = 'remote'
+    // force delete local branches
+    if (option === remote || option === 'both') {
+      // force delete remote branches
+      await git.push('origin', branch, ['-d']);
+      successMessage(branch, remote);
+    }
+  } catch (e) {
+    errorMessage(branch, remote, ' (or you are on it)');
+    // console.log(e)
   }
-  catch (e) { 
-    console.log('Error')
-    console.log(e)
+
+  const local = 'local'
+  try {
+    // force delete local branches
+    if (option === local || option === 'both') {
+      await git.deleteLocalBranch(branch, true);
+      successMessage(branch, local);
+    }
+  } catch (e) {
+    errorMessage(branch, local);
+    // console.log(e)
   }
 }
 
-for (const branch of remoteBranches) {
+for (const branch of branches) {
   // console.log(branch)
-  deleteBranch(branch)
+  deleteBranch(branch, option);
 }
-
-// deleteBranch()
